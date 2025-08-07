a9160689a8391846768aad362f9f0977d7409c93
James Glover <james.glover7@outlook.com>
2025-08-07 18:33:25 +0100

    Added buttons to switch to page to add pension and investments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,24 +8,26 @@ import {
   Box,
 } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import {useNavigate} from "react-router";
 export default function Header() {
-  return (<AppBar position="static" sx={{ bgcolor: "primary.main" }}>
-    {" "}
-    {/* Use common.black for pure black */}
-    <Toolbar>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        Retirement Planner
-      </Typography>
-      <Box sx={{ display: { xs: "none", md: "flex" } }}>
-        {" "}
-        {/* Hide on small screens if desired */}
-        <Button color="inherit">My snapshots</Button>
-        <Button color="inherit">My scenarios</Button>
-        <Button color="inherit">Learn more ?</Button>
-      </Box>
-      <IconButton size="large" edge="end" color="inherit" aria-label="account">
-        <AccountCircleIcon />
-      </IconButton>
-    </Toolbar>
-  </AppBar>);
+  const navigate = useNavigate();
+  return (
+      <AppBar position="static" sx={{ bgcolor: "primary.main" }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Retirement Planner
+          </Typography>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Button color="inherit" onClick={() => navigate("../portfolios")}>Portfolios</Button>
+            <Button color="inherit" onClick={() => navigate("../dc-pensions")}>My Pensions</Button>
+            <Button color="inherit">My snapshots</Button>
+            <Button color="inherit">My scenarios</Button>
+            <Button color="inherit">Learn more ?</Button>
+          </Box>
+          <IconButton size="large" edge="end" color="inherit" aria-label="account">
+            <AccountCircleIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+  );
 }
