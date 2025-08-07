31681a21fc1ed09544c87ead724493555d1dd2da
James Glover <james.glover7@outlook.com>
2025-08-07 18:39:55 +0100

    Added ability to return to the dashboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,18 +8,26 @@ import {
   Box,
 } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import {useNavigate} from "react-router";
+import {useNavigate, Link} from "react-router";
 export default function Header() {
   const navigate = useNavigate();
+
   return (
       <AppBar position="static" sx={{ bgcolor: "primary.main" }}>
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+              variant="h6"
+              onClick={() => navigate("/dashboard")}
+              sx={{
+                flexGrow: 1,
+                cursor: 'pointer'
+              }}
+          >
             Retirement Planner
           </Typography>
           <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <Button color="inherit" onClick={() => navigate("../portfolios")}>Portfolios</Button>
-            <Button color="inherit" onClick={() => navigate("../dc-pensions")}>My Pensions</Button>
+            <Button color="inherit" onClick={() => navigate("/portfolios")}>Portfolios</Button>
+            <Button color="inherit" onClick={() => navigate("/dc-pensions")}>My Pensions</Button>
             <Button color="inherit">My snapshots</Button>
             <Button color="inherit">My scenarios</Button>
             <Button color="inherit">Learn more ?</Button>
@@ -31,3 +39,4 @@ export default function Header() {
       </AppBar>
   );
 }
+
