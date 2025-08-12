c2939dd46e72611d8b66b27f8eaf72581b65db4e
James Glover <james.glover7@outlook.com>
2025-08-12 10:49:34 +0100

    Implemented logic so State Pension information is retrieved from the same api call as the one which populates the graph

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,244 +1,250 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-    Paper,
-    Tabs,
-    Tab,
-    Box,
-    Typography,
-    TextField,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    Container, CircularProgress
+  Paper,
+  Tabs,
+  Tab,
+  Box,
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Container, CircularProgress
 } from '@mui/material';
 import { Stack } from '@mui/system'; // For vertical stacking
-import { getMyInvestmentHeaders} from "../api/apiService.js";
+import {getMyInvestmentHeaders } from "../api/apiService.js";
 import WithdrawalStrategy from "./WithdrawalStrategy.jsx";
 import AnnuityStrategy from "./AnnuityStrategy.jsx";
 
 function Sidebar({
-                     retirementAge,
-                     setRetirementAge,
-                     percentageLumpsum,
-                     setPercentageLumpsum,
-                     incomeStrategy,
-                     setIncomeStrategy,
-                     withdrawalType,
-                     setWithdrawalType,
-                     initialAmount,
-                     setInitialAmount,
-                     increaseRate,
-                     setIncreaseRate,
-                     annuityType,
-                     setAnnuityType,
-                     annuityIncreaseRate,
-                     setAnnuityIncreaseRate
-                 })
-{
-    // Default active tab
-    const [activeTab, setActiveTab] = useState(0); // 0 for My savings, 1 for My options
-    const [investments, setInvestments] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError]=useState(null);
-    const [statePension, setStatePension]= useState({age:55, income:50000});
-    // const [percentageLumpsum.percenttageLumpsum, setPercentageLumpSum] = useState(25);
-    const [desiredIncome, setDesiredIncome] = useState(30000);
+  retirementAge,
+  setRetirementAge,
+  percentageLumpsum,
+  setPercentageLumpsum,
+  incomeStrategy,
+  setIncomeStrategy,
+  withdrawalType,
+  setWithdrawalType,
+  initialAmount,
+  setInitialAmount,
+  increaseRate,
+  setIncreaseRate,
+  annuityType,
+  setAnnuityType,
+  annuityIncreaseRate,
+  setAnnuityIncreaseRate,
+    statePensionAge,
+                   statePensionValue,
+    graphType
+}) {
+  // Default active tab
+  const [activeTab, setActiveTab] = useState(0); // 0 for My savings, 1 for My options
+  const [investments, setInvestments] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  // const [statePension, setStatePension] = useState({});
+  // const [percentageLumpsum.percenttageLumpsum, setPercentageLumpSum] = useState(25);
+  const [desiredIncome, setDesiredIncome] = useState(30000);
 
-    const handleTabChange = (event, newValue) => {
-        setActiveTab(newValue);
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
+
+  useEffect(() => {
+    const getInvestmentHeaders = async () => {
+      // console.log("Investment Headers is called!")
+      try {
+        setLoading(true);
+        const response = await getMyInvestmentHeaders();
+        // console.log(response);
+        setInvestments(response);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     };
+    getInvestmentHeaders();
+  }, []);
 
-    useEffect( () => {
-        const getInvestmentHeaders = async () => {
-            // console.log("Investment Headers is called!")
-            try {
-                setLoading(true);
-                const response = await getMyInvestmentHeaders();
-                // console.log(response);
-                setInvestments(response);
-            } catch (err) {
-                setError(err.message);
-            } finally {
-                setLoading(false);
-            }
-        };
-            getInvestmentHeaders();
-    },[]);
 
-    if (loading) {
-        return (
-            <Container sx={{display:'flex', justifyContent: 'center', mt: 4}}>
-                <CircularProgress/>
-            </Container>
-        );
-    }
-
-    if (error) {
-        return (
-            <Container>
-                {/*I might just instead remove this and have an error page when things go wrong. While developping that would be annoying so change that later.*/}
-                <Typography color="error">Error: {error}</Typography>
-            </Container>
-        );
-    }
-
+  if (loading) {
     return (
-        <Paper sx={{width: 280, p: 2, display: 'flex', flexDirection: 'column'}}>
-            <Tabs
-                value={activeTab}
-                onChange={handleTabChange}
-                variant="fullWidth"
-                sx={{mb: 2, bgcolor: 'background.default', borderRadius: 1}}
-            >
-                <Tab label="My savings" sx={{
-                    bgcolor: activeTab === 0 ? 'secondary.light' : 'transparent', // Light blue background for active
-                    color: activeTab === 0 ? 'primary.contrastText' : 'text.primary',
-                    border: '1px solid',
-                    borderColor: activeTab === 0 ? 'secondary.main' : 'grey.300',
-                    borderRight: activeTab === 0 ? 'none' : '1px solid grey.300',
-                    borderTopLeftRadius: 'inherit',
-                    borderBottomLeftRadius: 'inherit',
-                }}/>
-                <Tab label="My options" sx={{
-                    bgcolor: activeTab === 1 ? 'secondary.light' : 'transparent',
-                    color: activeTab === 1 ? 'primary.contrastText' : 'text.primary',
-                    border: '1px solid',
-                    borderColor: activeTab === 1 ? 'secondary.main' : 'grey.300',
-                    borderLeft: activeTab === 1 ? 'none' : '1px solid grey.300',
-                    borderTopRightRadius: 'inherit',
-                    borderBottomRightRadius: 'inherit',
-                }}/>
-            </Tabs>
-
-            <Box sx={{flexGrow: 1}}>
-                {activeTab === 0 && (
-                    <Stack spacing={2}>
-                        {investments.map((item) => (
-                            <Box
-                                key={item.id}
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                    pb: 1,
-                                    borderBottom: '1px solid',
-                                    borderColor: 'grey.300',
-                                }}
-                            >
-                                <Typography variant="body1" fontWeight="bold" color="text.secondary">
-                                    {item.name}
-                                </Typography>
-                                <Typography variant="body1">
-                                    £ {item.value}
-                                </Typography>
-                            </Box>
-                        ))}
-                        <Stack>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                    pb: 1,
-                                    // borderBottom: '1px solid',
-                                    borderColor: 'grey.300',
-                                }}
-                            >
-                                <Typography variant="body1" fontWeight="bold" color="text.secondary">
-                                    State pension income is
-                                </Typography>
-                                <Typography variant="body1">
-                                    £ {statePension.income}
-                                </Typography>
-                            </Box>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                    pb: 1,
-                                    borderBottom: '1px solid',
-                                    borderColor: 'grey.300',
-                                }}
-                            >
-                                <Typography variant="body1" fontWeight="bold" color="text.secondary">
-                                    from age
-                                </Typography>
-                                <Typography variant="body1">
-                                    {statePension.age}
-                                </Typography>
-                            </Box>
-                        </Stack>
-                    </Stack>
-                )}
-                {activeTab === 1 && (
-                    <Stack spacing={2}> {/* Stack components vertically with spacing */}
-                        <TextField
-                            label="Retirement Age"
-                            value={retirementAge}
-                            onChange={(e) => setRetirementAge(Number(e.target.value))}
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                            error={retirementAge < 57 || retirementAge > 120}
-                            helperText={retirementAge < 57 || retirementAge > 120 ? "Normal pension retirement age is at least 57" : ""}
-                        />
-                        <FormControl fullWidth variant="outlined" size="small">
-                            <InputLabel>Income strategy</InputLabel>
-                            <Select
-                                value={incomeStrategy}
-                                label="Income strategy"
-                                onChange={(e) => setIncomeStrategy(e.target.value)}
-                            >
-                                <MenuItem value="Annuity">Annuity</MenuItem>
-                                <MenuItem value="Withdrawal">Withdrawal</MenuItem>
-                            </Select>
-                        </FormControl>
-
-                        {incomeStrategy === 'Withdrawal' ? (
-                            <WithdrawalStrategy
-                                withdrawalType={withdrawalType}
-                                setWithdrawalType={setWithdrawalType}
-                                initialAmount={initialAmount}
-                                setInitialAmount={setInitialAmount}
-                                increaseRate={increaseRate}
-                                setIncreaseRate={setIncreaseRate}
-                            />
-                        ) : (
-                            <AnnuityStrategy
-                                annuityType={annuityType}
-                                setAnnuityType={setAnnuityType}
-                                annuityIncreaseRate={annuityIncreaseRate}
-                                setAnnuityIncreaseRate={setAnnuityIncreaseRate}
-                            />
-                        )}
-
-
-                        <TextField
-                            label="Lump sum at retirement"
-                            value={percentageLumpsum}
-                            onChange={(e) => setPercentageLumpsum(Number(e.target.value))}
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                        />
-                        <TextField
-                            label="Desired Annual Income"
-                            value={desiredIncome}
-                            onChange={(e) => setDesiredIncome(Number(e.target.value))}
-                            fullWidth
-                            variant="outlined"
-                            size="small"
-                            InputProps={{
-                                startAdornment: <Typography sx={{mr: 0.5}}>£</Typography>,
-                            }}
-                        />
-                    </Stack>
-                )}
-            </Box>
-        </Paper>
+      <Container sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+        <CircularProgress />
+      </Container>
     );
+  }
+
+  if (error) {
+    return (
+      <Container>
+        {/*I might just instead remove this and have an error page when things go wrong. While developping that would be annoying so change that later.*/}
+        <Typography color="error">Error: {error}</Typography>
+      </Container>
+    );
+  }
+
+  return (
+    <Paper sx={{ width: 280, p: 2, display: 'flex', flexDirection: 'column' }}>
+      <Tabs
+        value={activeTab}
+        onChange={handleTabChange}
+        variant="fullWidth"
+        sx={{ mb: 2, bgcolor: 'background.default', borderRadius: 1 }}
+      >
+        <Tab label="My savings" sx={{
+          bgcolor: activeTab === 0 ? 'secondary.light' : 'transparent', // Light blue background for active
+          color: activeTab === 0 ? 'primary.contrastText' : 'text.primary',
+          border: '1px solid',
+          borderColor: activeTab === 0 ? 'secondary.main' : 'grey.300',
+          borderRight: activeTab === 0 ? 'none' : '1px solid grey.300',
+          borderTopLeftRadius: 'inherit',
+          borderBottomLeftRadius: 'inherit',
+        }} />
+        <Tab label="My options" sx={{
+          bgcolor: activeTab === 1 ? 'secondary.light' : 'transparent',
+          color: activeTab === 1 ? 'primary.contrastText' : 'text.primary',
+          border: '1px solid',
+          borderColor: activeTab === 1 ? 'secondary.main' : 'grey.300',
+          borderLeft: activeTab === 1 ? 'none' : '1px solid grey.300',
+          borderTopRightRadius: 'inherit',
+          borderBottomRightRadius: 'inherit',
+        }} />
+      </Tabs>
+
+      <Box sx={{ flexGrow: 1 }}>
+        {activeTab === 0 && (
+          <Stack spacing={2}>
+            {investments.map((item) => (
+              <Box
+                key={item.id}
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  pb: 1,
+                  borderBottom: '1px solid',
+                  borderColor: 'grey.300',
+                }}
+              >
+                <Typography variant="body1" fontWeight="bold" color="text.secondary">
+                  {item.name}
+                </Typography>
+                <Typography variant="body1">
+                  £ {item.value}
+                </Typography>
+              </Box>
+            ))}
+            {graphType === "income" && (
+                <Stack>
+                  <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        pb: 1,
+                        // borderBottom: '1px solid',
+                        borderColor: 'grey.300',
+                      }}
+                  >
+                    <Typography variant="body1" fontWeight="bold" color="text.secondary">
+                      State pension income is
+                    </Typography>
+                    <Typography variant="body1">
+                      £ {Math.floor(Number(statePensionValue)).toLocaleString()}
+
+                    </Typography>
+                  </Box>
+                  <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        pb: 1,
+                        borderBottom: '1px solid',
+                        borderColor: 'grey.300',
+                      }}
+                  >
+                    <Typography variant="body1" fontWeight="bold" color="text.secondary">
+                      from age
+                    </Typography>
+                    <Typography variant="body1">
+                      {statePensionAge}
+                    </Typography>
+                  </Box>
+                </Stack>
+            )}
+          </Stack>
+        )}
+        {activeTab === 1 && (
+          <Stack spacing={2}> {/* Stack components vertically with spacing */}
+            <TextField
+              label="Retirement Age"
+              value={retirementAge}
+              onChange={(e) => setRetirementAge(Number(e.target.value))}
+              fullWidth
+              variant="outlined"
+              size="small"
+              error={retirementAge < 57 || retirementAge > 120}
+              helperText={retirementAge < 57 || retirementAge > 120 ? "Normal pension retirement age is at least 57" : ""}
+            />
+            <FormControl fullWidth variant="outlined" size="small">
+              <InputLabel>Income strategy</InputLabel>
+              <Select
+                value={incomeStrategy}
+                label="Income strategy"
+                onChange={(e) => setIncomeStrategy(e.target.value)}
+              >
+                <MenuItem value="Annuity">Annuity</MenuItem>
+                <MenuItem value="Withdrawal">Withdrawal</MenuItem>
+              </Select>
+            </FormControl>
+
+            {incomeStrategy === 'Withdrawal' ? (
+              <WithdrawalStrategy
+                withdrawalType={withdrawalType}
+                setWithdrawalType={setWithdrawalType}
+                initialAmount={initialAmount}
+                setInitialAmount={setInitialAmount}
+                increaseRate={increaseRate}
+                setIncreaseRate={setIncreaseRate}
+              />
+            ) : (
+              <AnnuityStrategy
+                annuityType={annuityType}
+                setAnnuityType={setAnnuityType}
+                annuityIncreaseRate={annuityIncreaseRate}
+                setAnnuityIncreaseRate={setAnnuityIncreaseRate}
+              />
+            )}
+
+
+            <TextField
+              label="Lump sum at retirement"
+              value={percentageLumpsum}
+              onChange={(e) => setPercentageLumpsum(Number(e.target.value))}
+              fullWidth
+              variant="outlined"
+              size="small"
+            />
+            <TextField
+              label="Desired Annual Income"
+              value={desiredIncome}
+              onChange={(e) => setDesiredIncome(Number(e.target.value))}
+              fullWidth
+              variant="outlined"
+              size="small"
+              InputProps={{
+                startAdornment: <Typography sx={{ mr: 0.5 }}>£</Typography>,
+              }}
+            />
+          </Stack>
+        )}
+      </Box>
+    </Paper>
+  );
 }
 
 export default Sidebar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,92 +3,103 @@ import NavigationTabs from "../components/NavigationTabs.jsx";
 import Sidebar from "../components/Sidebar.jsx";
 import MainContentArea from "../components/MainContentArea.jsx";
 import RightSidebar from "../components/RightSidebar.jsx";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import Footer from "../components/Footer.jsx";
-import {useState} from "react";
+import { useState } from "react";
 
 
 export default function Dashboard() {
-    console.log("Dashboard page 3");
-    const [outcomeValue, setOutcomeValue] = useState(50);
-    const [retirementAge, setRetirementAge] = useState(57);
-    const [percentageLumpsum, setPercentageLumpsum] = useState(25);
-    const [incomeStrategy, setIncomeStrategy] = useState('Annuity');
+  console.log("Dashboard page 3");
+  const [outcomeValue, setOutcomeValue] = useState(50);
+  const [retirementAge, setRetirementAge] = useState(57);
+  const [percentageLumpsum, setPercentageLumpsum] = useState(25);
+  const [incomeStrategy, setIncomeStrategy] = useState('Annuity');
 
-    const [withdrawalType, setWithdrawalType] = useState('fixed');
-    const [initialAmount, setInitialAmount] = useState('1000');
-    const [increaseRate, setIncreaseRate] = useState('3');
+  const [withdrawalType, setWithdrawalType] = useState('fixed');
+  const [initialAmount, setInitialAmount] = useState('1000');
+  const [increaseRate, setIncreaseRate] = useState('3');
 
-    const [annuityType, setAnnuityType] = useState('level');
-    const [annuityIncreaseRate, setAnnuityIncreaseRate] = useState('2');
+  const [annuityType, setAnnuityType] = useState('level');
+  const [annuityIncreaseRate, setAnnuityIncreaseRate] = useState('2');
+  const [statePensionAge, setStatePensionAge] = useState('67');
+  const [statePensionValue, setStatePensionValue] = useState('11973');
+  const [graphType, setGraphType] = useState('income');
 
 
 
-    const handleOutcomeChange = (event, newValue) => {
-        setOutcomeValue(newValue);
-    };
-    return (
+  const handleOutcomeChange = (event, newValue) => {
+    setOutcomeValue(newValue);
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        bgcolor: "background.default",
+      }}
+    >
+      <Header />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          p: 1.5, // Padding around the main content (1 unit = 8px by default)
+        }}
+      >
+        <NavigationTabs />
         <Box
-            sx={{
-                display: "flex",
-                flexDirection: "column",
-                minHeight: "100vh",
-                bgcolor: "background.default",
-            }}
+          sx={{
+            display: "flex",
+            flexGrow: 1,
+            gap: 1.5, // Space between columns
+            mt: 1.5, // Margin top from tabs
+          }}
         >
-            <Header />
-            <Box
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    flexGrow: 1,
-                    p: 1.5, // Padding around the main content (1 unit = 8px by default)
-                }}
-            >
-                <NavigationTabs />
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexGrow: 1,
-                        gap: 1.5, // Space between columns
-                        mt: 1.5, // Margin top from tabs
-                    }}
-                >
-                    <Sidebar
-                        retirementAge={retirementAge}
-                        setRetirementAge={setRetirementAge}
-                        percentageLumpsum={percentageLumpsum}
-                        setPercentageLumpsum={setPercentageLumpsum}
-                        incomeStrategy={incomeStrategy}
-                        setIncomeStrategy={setIncomeStrategy}
-                        withdrawalType={withdrawalType}
-                        setWithdrawalType={setWithdrawalType}
-                        initialAmount={initialAmount}
-                        setInitialAmount={setInitialAmount}
-                        increaseRate={increaseRate}
-                        setIncreaseRate={setIncreaseRate}
-                        annuityType={annuityType}
-                        setAnnuityType={setAnnuityType}
-                        annuityIncreaseRate={annuityIncreaseRate}
-                        setAnnuityIncreaseRate={setAnnuityIncreaseRate}
-                    />
+          <Sidebar
+            retirementAge={retirementAge}
+            setRetirementAge={setRetirementAge}
+            percentageLumpsum={percentageLumpsum}
+            setPercentageLumpsum={setPercentageLumpsum}
+            incomeStrategy={incomeStrategy}
+            setIncomeStrategy={setIncomeStrategy}
+            withdrawalType={withdrawalType}
+            setWithdrawalType={setWithdrawalType}
+            initialAmount={initialAmount}
+            setInitialAmount={setInitialAmount}
+            increaseRate={increaseRate}
+            setIncreaseRate={setIncreaseRate}
+            annuityType={annuityType}
+            setAnnuityType={setAnnuityType}
+            annuityIncreaseRate={annuityIncreaseRate}
+            setAnnuityIncreaseRate={setAnnuityIncreaseRate}
+            statePensionAge={statePensionAge}
+            statePensionValue={statePensionValue}
+            graphType={graphType}/>
 
-                    <MainContentArea outcomeValue={outcomeValue} retirementAge={retirementAge}
-                                     percentageLumpsum={percentageLumpsum}
-                                    incomeStrategy={incomeStrategy}
-                                    withdrawalType={withdrawalType}
-                                    initialAmount={initialAmount}
-                                    increaseRate={increaseRate}
-                                    annuityType={annuityType}
-                                    annuityIncreaseRate={annuityIncreaseRate}/>
-                    <RightSidebar
-                        outcomeValue={outcomeValue}
-                        setOutcomeValue={setOutcomeValue}
-                        handleOutcomeChange={handleOutcomeChange}
-                    />
-                </Box>
-            </Box>
-            <Footer />
+          <MainContentArea outcomeValue={outcomeValue} retirementAge={retirementAge}
+            percentageLumpsum={percentageLumpsum}
+            incomeStrategy={incomeStrategy}
+            withdrawalType={withdrawalType}
+            initialAmount={initialAmount}
+            increaseRate={increaseRate}
+            annuityType={annuityType}
+            annuityIncreaseRate={annuityIncreaseRate}
+            statePensionAge={statePensionAge}
+            statePensionValue={statePensionValue}
+            setStatePensionAge={setStatePensionAge}
+            setStatePensionValue={setStatePensionValue}
+            graphType={graphType}
+            setGraphType={setGraphType}/>
+          <RightSidebar
+            outcomeValue={outcomeValue}
+            setOutcomeValue={setOutcomeValue}
+            handleOutcomeChange={handleOutcomeChange}
+          />
         </Box>
-    );
+      </Box>
+      <Footer />
+    </Box>
+  );
 }
