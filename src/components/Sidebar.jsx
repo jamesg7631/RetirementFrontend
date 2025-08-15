dd02675b2a9b5fdd1f1e26e32a2d96799863f388
James Glover <james.glover7@outlook.com>
2025-08-15 15:13:41 +0100

    Fixed Desired Annual Income bug where you could enter a non valid value

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ import {
   Tooltip
 } from '@mui/material';
 import { Stack } from '@mui/system'; // For vertical stacking
-import {getMyInvestmentHeaders, deleteDcPension, getAllDcPensions} from "../api/apiService.js";
+import { getMyInvestmentHeaders, deleteDcPension, getAllDcPensions } from "../api/apiService.js";
 import WithdrawalStrategy from "./WithdrawalStrategy.jsx";
 import AnnuityStrategy from "./AnnuityStrategy.jsx";
 import AddDCPensionDialog from './AddDCPensionDialog';
@@ -23,22 +23,23 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 
-function Sidebar({retirementAge,
-                   percentageLumpsum,
-                   incomeStrategy,
-                   withdrawalType,
-                   initialAmount,
-                   increaseRate,
-                   annuityType,
-                   annuityIncreaseRate,
-                   statePensionAge,
-                   statePensionValue,
-                   graphType,
-                   onUpdate,
-                   exploredTab, 
-                   currentNavigationTab
+function Sidebar({ retirementAge,
+  percentageLumpsum,
+  incomeStrategy,
+  withdrawalType,
+  initialAmount,
+  increaseRate,
+  annuityType,
+  annuityIncreaseRate,
+  statePensionAge,
+  statePensionValue,
+  graphType,
+  onUpdate,
+  exploredTab,
+  desiredAnnualIncome,
+  currentNavigationTab
 
-                 }) {
+}) {
   // Default active tab
   const [activeTab, setActiveTab] = useState(0); // 0 for My savings, 1 for My options
   const [investments, setInvestments] = useState([]);
@@ -48,34 +49,35 @@ function Sidebar({retirementAge,
   const [openAddDCPensionDialog, setOpenAddDCPensionDialog] = useState(false);
   const [selectedPension, setSelectedPension] = useState(null);
   const [dialogMode, setDialogMode] = useState("add"); // "add" or "edit"
-  
+  const [desiredAnnualIncomeHelperText, setDesiredAnnualIncomeHelperText] = useState('');
+
   const handleOpenAddDCPension = () => {
     console.log("currentNavigationTab: " + currentNavigationTab);
     setDialogMode("add");
     setSelectedPension(null);
     setOpenAddDCPensionDialog(true);
   };
-  
+
   const handleOpenEditDCPension = async (pension) => {
     console.log("Sidebar edit pension: ", pension);
     setDialogMode("edit");
     setSelectedPension(pension);
     setOpenAddDCPensionDialog(true);
   };
-  
+
   const handleCloseAddDCPension = () => {
     setOpenAddDCPensionDialog(false);
     setSelectedPension(null);
 
   };
-  
+
   const handleDeletePension = async (pension) => {
     if (window.confirm(`Are you sure you want to delete ${pension.name}?`)) {
       try {
         const pathVariable = currentNavigationTab === 1 ? "explored" : "current";
         await deleteDcPension(pension.name, pathVariable);
         // Refresh the pension list after deletion
-        const updatedInvestments = await getAllDcPensions(pathVariable); 
+        const updatedInvestments = await getAllDcPensions(pathVariable);
         setInvestments(updatedInvestments);
       } catch (err) {
         console.error("Failed to delete pension:", err);
@@ -89,11 +91,26 @@ function Sidebar({retirementAge,
     setActiveTab(newValue);
   };
 
+  const handleInputChangeDesiredAnnualIncome = (event) => {
+    const newValue = event.target.value;
+    const isPositive = parseFloat(newValue) >= 0 || newValue === '';
+
+    setValue(newValue);
+
+    if (!isPositive) {
+      setError(true);
+      setDesiredAnnualIncomeHelperText('Please enter a positive number');
+    } else {
+      setError(false);
+      setHelperText('');
+    }
+  }
+
   useEffect(() => {
     const getInvestmentHeaders = async () => {
       // console.log("Investment Headers is called!")
       if (openAddDCPensionDialog == true) {
-            return;
+        return;
       }
       try {
         setLoading(true);
@@ -130,101 +147,101 @@ function Sidebar({retirementAge,
   }
 
   return (
-      <>
-    <Paper sx={{ width: 280, p: 2, display: 'flex', flexDirection: 'column' }}>
-      <Tabs
-        value={activeTab}
-        onChange={handleTabChange}
-        variant="fullWidth"
-        sx={{ mb: 2, bgcolor: 'background.default', borderRadius: 1 }}
-      >
-        <Tab label="My savings" sx={{
-          bgcolor: activeTab === 0 ? 'secondary.light' : 'transparent', // Light blue background for active
-          color: activeTab === 0 ? 'primary.contrastText' : 'text.primary',
-          border: '1px solid',
-          borderColor: activeTab === 0 ? 'secondary.main' : 'grey.300',
-          borderRight: activeTab === 0 ? 'none' : '1px solid grey.300',
-          borderTopLeftRadius: 'inherit',
-          borderBottomLeftRadius: 'inherit',
-        }} />
-        <Tab label="My options" sx={{
-          bgcolor: activeTab === 1 ? 'secondary.light' : 'transparent',
-          color: activeTab === 1 ? 'primary.contrastText' : 'text.primary',
-          border: '1px solid',
-          borderColor: activeTab === 1 ? 'secondary.main' : 'grey.300',
-          borderLeft: activeTab === 1 ? 'none' : '1px solid grey.300',
-          borderTopRightRadius: 'inherit',
-          borderBottomRightRadius: 'inherit',
-        }} />
-      </Tabs>
+    <>
+      <Paper sx={{ width: 280, p: 2, display: 'flex', flexDirection: 'column' }}>
+        <Tabs
+          value={activeTab}
+          onChange={handleTabChange}
+          variant="fullWidth"
+          sx={{ mb: 2, bgcolor: 'background.default', borderRadius: 1 }}
+        >
+          <Tab label="My savings" sx={{
+            bgcolor: activeTab === 0 ? 'secondary.light' : 'transparent', // Light blue background for active
+            color: activeTab === 0 ? 'primary.contrastText' : 'text.primary',
+            border: '1px solid',
+            borderColor: activeTab === 0 ? 'secondary.main' : 'grey.300',
+            borderRight: activeTab === 0 ? 'none' : '1px solid grey.300',
+            borderTopLeftRadius: 'inherit',
+            borderBottomLeftRadius: 'inherit',
+          }} />
+          <Tab label="My options" sx={{
+            bgcolor: activeTab === 1 ? 'secondary.light' : 'transparent',
+            color: activeTab === 1 ? 'primary.contrastText' : 'text.primary',
+            border: '1px solid',
+            borderColor: activeTab === 1 ? 'secondary.main' : 'grey.300',
+            borderLeft: activeTab === 1 ? 'none' : '1px solid grey.300',
+            borderTopRightRadius: 'inherit',
+            borderBottomRightRadius: 'inherit',
+          }} />
+        </Tabs>
 
-      <Box sx={{ flexGrow: 1 }}>
-        {activeTab === 0 && (
-          <Stack spacing={2}>
-            <Button
+        <Box sx={{ flexGrow: 1 }}>
+          {activeTab === 0 && (
+            <Stack spacing={2}>
+              <Button
                 variant="contained"
                 color="primary"
                 onClick={handleOpenAddDCPension}
                 sx={{ mb: 2 }}
-            >
-              Add DC Pension
-            </Button>
-            {/*Start of edit section*/}
-            {investments.map((item) => (
-              <Box
-                key={item.id}
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                  pb: 1,
-                  borderBottom: '1px solid',
-                  borderColor: 'grey.300',
-                }}
               >
-                <Typography variant="body1" fontWeight="bold" color="text.secondary">
-                  {item.name}
-                </Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography variant="body1" sx={{ mr: 1 }}>
-                    £ {item.currentValue}
+                Add DC Pension
+              </Button>
+              {/*Start of edit section*/}
+              {investments.map((item) => (
+                <Box
+                  key={item.id}
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    pb: 1,
+                    borderBottom: '1px solid',
+                    borderColor: 'grey.300',
+                  }}
+                >
+                  <Typography variant="body1" fontWeight="bold" color="text.secondary">
+                    {item.name}
                   </Typography>
-                  <Tooltip title="Edit">
-                    <IconButton 
-                      size="small" 
-                      color="primary" 
-                      onClick={() => handleOpenEditDCPension(item)}
-                      sx={{ p: 0.5 }}
-                    >
-                      <EditIcon fontSize="small" />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Delete">
-                    <IconButton 
-                      size="small" 
-                      color="error" 
-                      onClick={() => handleDeletePension(item)}
-                      sx={{ p: 0.5 }}
-                    >
-                      <DeleteIcon fontSize="small" />
-                    </IconButton>
-                  </Tooltip>
+                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography variant="body1" sx={{ mr: 1 }}>
+                      £ {item.currentValue}
+                    </Typography>
+                    <Tooltip title="Edit">
+                      <IconButton
+                        size="small"
+                        color="primary"
+                        onClick={() => handleOpenEditDCPension(item)}
+                        sx={{ p: 0.5 }}
+                      >
+                        <EditIcon fontSize="small" />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Delete">
+                      <IconButton
+                        size="small"
+                        color="error"
+                        onClick={() => handleDeletePension(item)}
+                        sx={{ p: 0.5 }}
+                      >
+                        <DeleteIcon fontSize="small" />
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
                 </Box>
-              </Box>
-            ))}
+              ))}
 
-            {/*End of edit section*/}
-            {graphType === "income" && (
+              {/*End of edit section*/}
+              {graphType === "income" && (
                 <Stack>
                   <Box
-                      sx={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        pb: 1,
-                        // borderBottom: '1px solid',
-                        borderColor: 'grey.300',
-                      }}
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      pb: 1,
+                      // borderBottom: '1px solid',
+                      borderColor: 'grey.300',
+                    }}
                   >
                     <Typography variant="body1" fontWeight="bold" color="text.secondary">
                       State pension income is
@@ -235,14 +252,14 @@ function Sidebar({retirementAge,
                     </Typography>
                   </Box>
                   <Box
-                      sx={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        pb: 1,
-                        borderBottom: '1px solid',
-                        borderColor: 'grey.300',
-                      }}
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      pb: 1,
+                      borderBottom: '1px solid',
+                      borderColor: 'grey.300',
+                    }}
                   >
                     <Typography variant="body1" fontWeight="bold" color="text.secondary">
                       from age
@@ -252,85 +269,86 @@ function Sidebar({retirementAge,
                     </Typography>
                   </Box>
                 </Stack>
-            )}
-          </Stack>
-        )}
-        {activeTab === 1 && (
-          <Stack spacing={2}> {/* Stack components vertically with spacing */}
-            <TextField
-              label="Retirement Age"
-              value={retirementAge}
-              // onChange={(e) => setRetirementAge(Number(e.target.value))}
-                onChange={(e) => onUpdate({retirementAge: Number(e.target.value)})}
-              fullWidth
-              variant="outlined"
-              size="small"
-              error={retirementAge < 57 || retirementAge > 120}
-              helperText={retirementAge < 57 || retirementAge > 120 ? "Normal pension retirement age is at least 57" : ""}
-            />
-            <FormControl fullWidth variant="outlined" size="small">
-              <InputLabel>Income strategy</InputLabel>
-              <Select
-                value={incomeStrategy}
-                label="Income strategy"
-                onChange={(e) => onUpdate({incomeStrategy: e.target.value})}
-              >
-                <MenuItem value="Annuity">Annuity</MenuItem>
-                <MenuItem value="Withdrawal">Withdrawal</MenuItem>
-              </Select>
-            </FormControl>
-
-            {incomeStrategy === 'Withdrawal' ? (
-              <WithdrawalStrategy
-                withdrawalType={withdrawalType}
-                initialAmount={initialAmount}
-                increaseRate={increaseRate}
-                onUpdate={onUpdate}
+              )}
+            </Stack>
+          )}
+          {activeTab === 1 && (
+            <Stack spacing={2}> {/* Stack components vertically with spacing */}
+              <TextField
+                label="Retirement Age"
+                value={retirementAge}
+                // onChange={(e) => setRetirementAge(Number(e.target.value))}
+                onChange={(e) => onUpdate({ retirementAge: Number(e.target.value) })}
+                fullWidth
+                variant="outlined"
+                size="small"
+                error={retirementAge < 57 || retirementAge > 120}
+                helperText={retirementAge < 57 || retirementAge > 120 ? "Normal pension retirement age is at least 57" : ""}
               />
-            ) : (
-              <AnnuityStrategy
-                annuityType={annuityType}
-                annuityIncreaseRate={annuityIncreaseRate}
-                onUpdate={onUpdate}
-              />
-            )}
+              <FormControl fullWidth variant="outlined" size="small">
+                <InputLabel>Income strategy</InputLabel>
+                <Select
+                  value={incomeStrategy}
+                  label="Income strategy"
+                  onChange={(e) => onUpdate({ incomeStrategy: e.target.value })}
+                >
+                  <MenuItem value="Annuity">Annuity</MenuItem>
+                  <MenuItem value="Withdrawal">Withdrawal</MenuItem>
+                </Select>
+              </FormControl>
 
-
-            <TextField
-              label="Lump sum at retirement"
-              value={percentageLumpsum}
-              // onChange={(e) => setPercentageLumpsum(Number(e.target.value))}
-                onChange={(e) => onUpdate({percentageLumpsum: Number(e.target.value)})}
-              fullWidth
-              variant="outlined"
-              size="small"
-            />
-            {graphType === "income" && (
-                <TextField
-                    label="Desired Annual Income"
-                    value={desiredIncome}
-                    // onChange={(e) => setDesiredIncome(Number(e.target.value))}
-                    onChange={(e) => onUpdate({desiredIncome: Number(e.target.value)})}
-                    fullWidth
-                    variant="outlined"
-                    size="small"
-                    InputProps={{
-                      startAdornment: <Typography sx={{ mr: 0.5 }}>£</Typography>,
-                    }}
+              {incomeStrategy === 'Withdrawal' ? (
+                <WithdrawalStrategy
+                  withdrawalType={withdrawalType}
+                  initialAmount={initialAmount}
+                  increaseRate={increaseRate}
+                  onUpdate={onUpdate}
                 />
-            )}
-          </Stack>
-        )}
-      </Box>
-    </Paper>
-  <AddDCPensionDialog
-      open={openAddDCPensionDialog}
-      onClose={handleCloseAddDCPension}
-      selectedPension={selectedPension}
-      mode={dialogMode}
-      currentTab={currentNavigationTab}
-  />
-        </>
+              ) : (
+                <AnnuityStrategy
+                  annuityType={annuityType}
+                  annuityIncreaseRate={annuityIncreaseRate}
+                  onUpdate={onUpdate}
+                />
+              )}
+
+
+              <TextField
+                label="Lump sum at retirement"
+                value={percentageLumpsum}
+                // onChange={(e) => setPercentageLumpsum(Number(e.target.value))}
+                onChange={(e) => onUpdate({ percentageLumpsum: Number(e.target.value) })}
+                fullWidth
+                variant="outlined"
+                size="small"
+              />
+              {graphType === "income" && (
+                <TextField
+                  label="Desired Annual Income"
+                  value={desiredAnnualIncome}
+                  onChange={(e) => onUpdate({ desiredAnnualIncome: Number(e.target.value) })}
+                  fullWidth
+                  error={error}
+                  helperText={desiredAnnualIncomeHelperText}
+                  variant="outlined"
+                  size="small"
+                  InputProps={{
+                    startAdornment: <Typography sx={{ mr: 0.5 }}>£</Typography>,
+                  }}
+                />
+              )}
+            </Stack>
+          )}
+        </Box>
+      </Paper>
+      <AddDCPensionDialog
+        open={openAddDCPensionDialog}
+        onClose={handleCloseAddDCPension}
+        selectedPension={selectedPension}
+        mode={dialogMode}
+        currentTab={currentNavigationTab}
+      />
+    </>
   );
 }
 
