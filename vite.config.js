b78f674137c170d1f6e59f2ecabde8224815c43c
James Glover <james.glover7@outlook.com>
2025-08-19 12:24:53 +0100

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Essential for Heroku deployments of SPAs to ensure correct asset paths
-  base: '/',
+  base: process.env.VITE_BASE_PATH || '/RetirementFrontend',
   build: {
     // Vite's default output directory for built static files
     outDir: 'dist',
